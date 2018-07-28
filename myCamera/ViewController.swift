f7132789a7fe4dff621a54c6508ec18d86d1d1fa
Yako Kobayashi <developer@cleanbuild.jp>
2018-07-28 15:57:03 +0900

    Initial Commit

---
+++ myCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myCamera
+//
+//  Created by Yako Kobayashi on 2018/07/28.
+//  Copyright © 2018年 yako Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
